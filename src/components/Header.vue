c7ada638fc93d5d80e1bb0e1c55c9d046a098eef
Mavlon4658 <mavlonbe4658>
2024-08-23 16:23:46 +0500

    home desktop is ready

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header class="header">
+        <div class="main_container header__container">
+            <div class="header__left">
+                <a href="/" class="header__logo">
+                    <img src="@/assets/images/logo.svg" alt="">
+                    <span class="header__logo_text">uniseller<span>.</span>io</span>
+                </a>
+                <ul class="header__nav">
+                    <li class="header__nav_item">
+                        <a href="#" class="header__nav_link">Услуги</a>
+                    </li>
+                    <li class="header__nav_item">
+                        <a href="#" class="header__nav_link">Интеграции</a>
+                    </li>
+                    <li class="header__nav_item">
+                        <a href="#" class="header__nav_link">Маркетплейсы</a>
+                    </li>
+                    <li class="header__nav_item">
+                        <a href="#" class="header__nav_link">Тарифы</a>
+                    </li>
+                    <li class="header__nav_item">
+                        <a href="#" class="header__nav_link">FAQ</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="header__right">
+                <button class="btn_orange">Оставить заявку</button>
+                <ul class="header__auth">
+                    <li>
+                        <a href="#">Войти</a>
+                    </li>
+                    <li>
+                        <a href="#">Регистрация</a>
+                    </li>
+                </ul>
+                <div class="header__settings">
+                    <a href="#">
+                        <img src="@/assets/images/user-circle.svg" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="@/assets/images/settings-icon.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,75 @@
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+export default {
+    name: 'Home',
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Autoplay, Pagination, Navigation],
+        };
+    },
+    methods: {
+        getUrl (name) {
+            return new URL(name, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <!-- Home -->
+    <section class="home">
+        <div class="main_container home__container">
+            <div class="home__left">
+                <img src="@/assets/images/logo-xl.svg" alt="">
+                <h2>uniseller<span>.</span>io</h2>
+                <p>автоматизация бизнеса на маркетплейсах</p>
+            </div>
+            <div class="home__right">
+                <h3>Все необходимое <br>для селлеров <br> <span>Маркетплейсов</span></h3>
+                <p>Уникальный инструмент, способный объеденить в одном окне упаравление продажами  на всех доступных маркетплейсах. Добавляйте организации, подключайте каналы продаж, создавайте карточки товаров, добавляйте ассортимент, контролируйте цены,  заказы и отгрузки. <br> С помощью Uniseller вы сможете масштабировать продажи и работать <br>с любым количеством личных кабинетов. Получите возможность подключения к “1С” , “Мой Склад” либо с другим вашим сервисом.</p>
+                <a href="#" class="btn_yellow">
+                    <span>Подробнее о UniSeller</span>
+                    <img src="@/assets/images/youtube.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </section>
+    <!-- Home end -->
+
+    <!-- Partners -->
+    <section class="partners">
+        <swiper
+            :slidesPerView="'auto'"
+            :spaceBetween="53"
+            :autoplay="{
+                delay: 2500,
+                disableOnInteraction: false,
+            }"
+            :modules="modules"
+            class="mySwiper"
+        >
+            <swiper-slide v-for="i in 5" :key="i">
+                <img :src="require('@/assets/images/partner-1-'+i+'.png')" alt="">
+            </swiper-slide>
+        </swiper>
+    </section>
+    <!-- Partners end -->
+</template>
+
+<style>
+
+</style>
