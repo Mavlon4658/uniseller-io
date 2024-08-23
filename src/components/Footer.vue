937b141030598f8c5ea951ab86031adf50a978f4
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-08-23 22:28:39 +0500

    footer desktop the end

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="main_container">
-      <div class="fotoer_top">
+      <div class="footer_top">
         <div class="footer_logo">
           <a href="/">
             <img src="@/assets/images/footer_logo.svg" alt="">
@@ -37,6 +37,83 @@
               </a>
             </div>
           </div>
+        </div>
+        <div class="footer_blocks">
+          <ul class="footer_link">
+            <h6>
+              Интеграции
+            </h6>
+            <li>
+              <a href="#">
+                Маркетплейсы
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Мой Склад
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                1 С
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Сторонние ресурсы
+              </a>
+            </li>
+          </ul>
+          <ul class="footer_link">
+            <h6>
+              Ресурсы
+            </h6>
+            <li>
+              <a href="#">
+                Блог
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Youtube
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Статьи
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Поддержка
+              </a>
+            </li>
+          </ul>
+          <ul class="footer_link">
+            <h6>
+              Общая информация
+            </h6>
+            <li>
+              <a href="#">
+                Пользовательское соглашение
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                О защите персональных данных
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Полезная информация
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Авторское право
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       <div class="footer_bottom">
