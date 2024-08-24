b08691ae33b3ffe5cba4791bdbef717383ae79ca
Mavlon4658 <mavlonbe4658>
2024-08-24 14:51:46 +0500

    mobile is ready

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,19 +14,129 @@ export default {
                 </a>
                 <ul class="header__nav">
                     <li class="header__nav_item">
-                        <a href="#" class="header__nav_link">Услуги</a>
+                        <a href="#" class="header__nav_link">
+                            <span>Услуги</span>
+                            <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.883789 1L4.38379 4L7.88379 0.999999" stroke="#EF6C1E" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                        <ul class="header__nav_list">
+                            <li>
+                                <a href="#">Личные кабинеты</a>
+                            </li>
+                            <li>
+                                <a href="#">Цены и остатки</a>
+                            </li>
+                            <li>
+                                <a href="#">Ассортимент</a>
+                            </li>
+                            <li>
+                                <a href="#">Подключение</a>
+                            </li>
+                            <li>
+                                <a href="#">Аналитика</a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="header__nav_item">
-                        <a href="#" class="header__nav_link">Интеграции</a>
+                        <a href="#" class="header__nav_link">
+                            <span>Интеграции</span>
+                            <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.883789 1L4.38379 4L7.88379 0.999999" stroke="#EF6C1E" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                        <ul class="header__nav_list">
+                            <li>
+                                <a href="#">1С</a>
+                            </li>
+                            <li>
+                                <a href="#">Мой склад</a>
+                            </li>
+                            <li>
+                                <a href="#">API</a>
+                            </li>
+                            <li>
+                                <a href="#">Решения</a>
+                            </li>
+                            <li>
+                                <a href="#">Сторонние сервисы</a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="header__nav_item">
-                        <a href="#" class="header__nav_link">Маркетплейсы</a>
+                        <a href="#" class="header__nav_link">
+                            <span>Маркетплейсы</span>
+                            <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.883789 1L4.38379 4L7.88379 0.999999" stroke="#EF6C1E" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                        <ul class="header__nav_list">
+                            <li>
+                                <a href="#">Ozon</a>
+                            </li>
+                            <li>
+                                <a href="#">Яндекс Маркет</a>
+                            </li>
+                            <li>
+                                <a href="#">Мегамаркет</a>
+                            </li>
+                            <li>
+                                <a href="#">Wildberries</a>
+                            </li>
+                            <li>
+                                <a href="#">Avito</a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="header__nav_item">
-                        <a href="#" class="header__nav_link">Тарифы</a>
+                        <a href="#" class="header__nav_link">
+                            <span>Тарифы</span>
+                            <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.883789 1L4.38379 4L7.88379 0.999999" stroke="#EF6C1E" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                        <ul class="header__nav_list">
+                            <li>
+                                <a href="#">Basic</a>
+                            </li>
+                            <li>
+                                <a href="#">Seller</a>
+                            </li>
+                            <li>
+                                <a href="#">SellerPro</a>
+                            </li>
+                            <li>
+                                <a href="#">SellerTop</a>
+                            </li>
+                            <li>
+                                <a href="#">MegaSeller</a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="header__nav_item">
-                        <a href="#" class="header__nav_link">FAQ</a>
+                        <a href="#" class="header__nav_link">
+                            <span>FAQ</span>
+                            <svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.883789 1L4.38379 4L7.88379 0.999999" stroke="#EF6C1E" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </a>
+                        <ul class="header__nav_list">
+                            <li>
+                                <a href="#">Личные кабинеты</a>
+                            </li>
+                            <li>
+                                <a href="#">Цены и остатки</a>
+                            </li>
+                            <li>
+                                <a href="#">Ассортимент</a>
+                            </li>
+                            <li>
+                                <a href="#">Подключение</a>
+                            </li>
+                            <li>
+                                <a href="#">Аналитика</a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </div>
@@ -48,6 +158,9 @@ export default {
                         <img src="@/assets/images/settings-icon.svg" alt="">
                     </a>
                 </div>
+                <button class="bars">
+                    <img src="@/assets/images/bars.svg" alt="">
+                </button>
             </div>
         </div>
     </header>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,7 +39,7 @@ export default {
                 <p>автоматизация бизнеса на маркетплейсах</p>
             </div>
             <div class="home__right">
-                <h3>Все необходимое <br>для селлеров <br> <span>Маркетплейсов</span></h3>
+                <h3>Все необходимое <br>для селлеров <span>Маркетплейсов</span></h3>
                 <p>Уникальный инструмент, способный объеденить в одном окне упаравление продажами  на всех доступных маркетплейсах. Добавляйте организации, подключайте каналы продаж, создавайте карточки товаров, добавляйте ассортимент, контролируйте цены,  заказы и отгрузки. <br> С помощью Uniseller вы сможете масштабировать продажи и работать <br>с любым количеством личных кабинетов. Получите возможность подключения к “1С” , “Мой Склад” либо с другим вашим сервисом.</p>
                 <a href="#" class="btn_yellow">
                     <span>Подробнее о UniSeller</span>
@@ -52,22 +52,58 @@ export default {
 
     <!-- Partners -->
     <section class="partners">
-        <swiper
-            :slidesPerView="'auto'"
-            :spaceBetween="53"
-            :autoplay="{
-                delay: 2500,
-                disableOnInteraction: false,
-            }"
-            :modules="modules"
-            class="mySwiper"
-        >
-            <swiper-slide v-for="i in 5" :key="i">
-                <img :src="require('@/assets/images/partner-1-'+i+'.png')" alt="">
-            </swiper-slide>
-        </swiper>
+        <div class="main_container partners__container">
+            <div class="partners_1">
+                <img v-for="i in 5" :key="i" :src="require('@/assets/images/partners-1-'+i+'.svg')" alt="">
+                <img v-for="i in 5" :key="i" :src="require('@/assets/images/partners-1-'+i+'.svg')" alt="">
+            </div>
+            <div class="partners_2">
+                <img v-for="i in 5" :key="i" :src="require('@/assets/images/partners-2-'+i+'.svg')" alt="">
+                <img v-for="i in 5" :key="i" :src="require('@/assets/images/partners-2-'+i+'.svg')" alt="">
+            </div>
+        </div>
     </section>
     <!-- Partners end -->
+
+    <!-- Cards -->
+    <section class="cards">
+        <div class="main_container cards__container">
+            <div class="cards_item">
+                <img src="@/assets/images/cards-1.svg" alt="" class="main-img">
+                <img src="@/assets/images/card-bg.png" class="cards_item__bg" alt="">
+                <h3>Все в одном окне</h3>
+                <p>Готовая интеграция со всеми <br> Маркетпейсами, 1С и Мой Склад. <br> Возможность подключения до <br> 200 личных кабинетов.</p>
+            </div>
+            <div class="cards_item">
+                <img src="@/assets/images/cards-2.svg" alt="" class="main-img">
+                <img src="@/assets/images/card-bg.png" class="cards_item__bg" alt="">
+                <h3>Цены и остатки</h3>
+                <p>Мгновенная передача остатков <br> и цен. Уникальная технология <br> контроля количества заказов <br> и резерв позиций. </p>
+            </div>
+            <div class="cards_item">
+                <img src="@/assets/images/cards-3.svg" alt="" class="main-img">
+                <img src="@/assets/images/card-bg.png" class="cards_item__bg" alt="">
+                <h3>Оптимизация <br> отгрузки</h3>
+                <p>Ускореный способ поиска <br> позиций и комплектации <br> заказов. Изменение <br>и контроль статусов.</p>
+            </div>
+            <div class="cards_item">
+                <img src="@/assets/images/cards-4.svg" alt="" class="main-img">
+                <img src="@/assets/images/card-bg.png" class="cards_item__bg" alt="">
+                <h3>Высокая <br> скорость работы <br> и надежность</h3>
+                <p>Минимальный процент <br> ошибок и отмен.</p>
+            </div>
+            <div class="cards_item">
+                <img src="@/assets/images/cards-5.svg" alt="" class="main-img">
+                <img src="@/assets/images/card-bg.png" class="cards_item__bg" alt="">
+                <h3>Сервис для <br> фулфилмент</h3>
+                <p>Выгружайте маркировочные <br> листы и реестры, осуществляйте <br> отгрузки для всех ваших клиентов.</p>
+            </div>
+            <div class="cards_last">
+                <img src="@/assets/images/card-6.svg" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- Cards end -->
 
     <!-- contact -->
     <section class="contact">
@@ -93,7 +129,7 @@ export default {
                                     <img src="@/assets/images/flag_icon.svg" alt="">
                                     <img src="@/assets/images/down_icon.svg" alt="">
                                 </div>
-                                <input type="number" placeholder="+7 (000) 000-00-00">
+                                <input type="tel" placeholder="+7 (000) 000-00-00">
                             </div>
                         </div>
                     </div>
